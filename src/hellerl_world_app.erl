0edffb2e2c549ecf5442af0c397e46caa4f7851e
Philip Cristiano <git@philipcristiano.com>
2020-03-04 08:00:45 -0500

    erlang: Bootstrap with mk

---
+++ src/hellerl_world_app.erl
@@ -0,0 +1,11 @@
+-module(hellerl_world_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	hellerl_world_sup:start_link().
+
+stop(_State) ->
+	ok.
