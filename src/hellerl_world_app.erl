1511a320a3b689e6b73db465e4f59951b2bbaed0
Philip Cristiano <git@philipcristiano.com>
2020-03-24 19:31:22 -0400

    wip: Add kube health stuff

--- src/hellerl_world_app.erl
+++ src/hellerl_world_app.erl
@@ -1,10 +1,23 @@
 -module(hellerl_world_app).
 -behaviour(application).
+-include_lib("kernel/include/logger.hrl").
 
 -export([start/2]).
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    ?LOG_INFO(#{what=><<"Starting Hellerl World">>}),
+    App = hellerl_world,
+    Dispatch = cowboy_router:compile([
+      {'_', [
+        {"/ready", hellerl_world_handler_ready, []},
+        {"/alive", hellerl_world_handler_alive, []},
+        {"/[...]", cowboy_static, {priv_dir, App, "public"}}
+      ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+      env => #{dispatch => Dispatch}
+    }),
     hellerl_world_sup:start_link().
 
 stop(_State) ->
