0134ddc4610f26d868446d3e9ead27488d1749a7
Philip Cristiano <git@philipcristiano.com>
2020-03-28 10:32:24 -0400

    log handling: Log data and count length
    So I can see what the fluent-bit logs look like in the cluster

--- src/hellerl_world_handler_incoming.erl
+++ src/hellerl_world_handler_incoming.erl
@@ -1,6 +1,8 @@
 -module(hellerl_world_handler_incoming).
 -behaviour(cowboy_route_setup).
 
+-include_lib("kernel/include/logger.hrl").
+
 -export([init/2,
          setup/0]).
 
@@ -16,11 +18,15 @@ init(Req0 = #{method := <<"POST">>,
               headers := #{<<"content-type">> := <<"application/json">>}},
             Opts) ->
     {ok, DataBin, Req1} = cowboy_req:read_body(Req0),
-    _Data = jsx:decode(DataBin, [return_maps]),
+    Data = jsx:decode(DataBin, [return_maps]),
+    Len = length(Data),
+    ?LOG_INFO(#{what => "Incoming log message received",
+            data => Data,
+            length => Len}),
     Rep = cowboy_req:reply(200, #{
         <<"content-type">> => <<"text/plain">>
     }, <<"POST">> , Req1),
-    prometheus_counter:inc(?M_INCOMING_LOGS_HANDLED),
+    prometheus_counter:inc(?M_INCOMING_LOGS_HANDLED, [], Len),
     {ok, Rep, Opts};
 init(Req0, Opts) ->
     Msg = <<"Requests should be posted with content-type: application/json">>,
