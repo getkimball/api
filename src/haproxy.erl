1c57d8fb1f2bbb321857e9df899e62a4a2847048
Philip Cristiano <git@philipcristiano.com>
2020-04-27 08:51:54 -0400

    haproxy: Add retry functionality
    Some requests to get frontends from HAProxy are reported as closed. I
    don't know why yet but it would be helpful to retry, at least in the
    case of GETs

--- src/haproxy.erl
+++ src/haproxy.erl
@@ -1,4 +1,5 @@
 -module(haproxy).
+-include_lib("kernel/include/logger.hrl").
 
 -export([frontends/0,
          request/2]).
@@ -12,8 +13,25 @@ frontends() ->
     User = <<"dataplaneapi">>,
     Pass = <<"insecure-password">>,
 
-    {ok, _Code, _Headers, Ref} = hackney:request(get, URL, [], [],
-                                                 [{basic_auth, {User, Pass}}]),
+    {ok, _Code, _Headers, Ref} = request_with_retry(get, URL, [], [],
+                                                 [{basic_auth, {User, Pass}}],
+                                                 3),
     {ok, Body} = hackney:body(Ref),
     Data = jsx:decode(Body, [return_maps]),
     Data.
+
+
+request_with_retry(_Method, _URL, _H, _P, _Opts, 0) ->
+    {error, no_more_retries};
+request_with_retry(Method, URL, H, P, Opts, Retries) ->
+    case hackney:request(Method, URL, H, P, Opts) of
+      {error, Reason} ->
+          ?LOG_INFO(#{what=>"Request Error",
+                       url=>URL,
+                       headers=>H,
+                       payload=>P,
+                       reason=>Reason,
+                       retries_left=>Retries - 1}),
+          request_with_retry(Method, URL, H, P, Opts, Retries -1);
+      Else -> Else
+    end.
