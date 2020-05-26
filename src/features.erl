68803d4b94e75eb455cc397f6efc1fedc685617f
Philip Cristiano <git@philipcristiano.com>
2020-05-26 08:57:18 -0400

    rollout: logic for in-progress rollout collapse

--- src/features.erl
+++ src/features.erl
@@ -1,27 +1,48 @@
 -module(features).
 
--export([collapse_to_boolean/2,
+-export([collapse_to_boolean/3,
          collapse_features_map/1]).
 
 collapse_to_boolean(#{boolean := Boolean,
-                      rollout_end := undefined}, _Now) ->
+                      rollout_end := undefined},
+                    _Now,
+                    _Rand) ->
     Boolean;
 collapse_to_boolean(#{rollout_start := Start,
-                      rollout_end := End}, Now) ->
+                      rollout_end := End},
+                    Now,
+                    Rand) ->
     Boolean = if
+        % Before start of rollout
         Now =< Start -> false;
-        Now >= End -> true
+        % After end of rollout
+        Now >= End -> true;
+        % During rollout
+        true -> collapse_rollout_progress(
+                  Start, End, Now, Rand)
     end,
     Boolean.
+
+collapse_features_map(Map) ->
+    Now = erlang:system_time(seconds),
+    Rand = rand:uniform(),
+    CollapseFun = fun(K, V, AccIn) ->
+      NewV = collapse_to_boolean(V, Now, Rand),
+      maps:put(K, NewV, AccIn)
+    end,
+    maps:fold(CollapseFun, #{}, Map).
 
 %%%%
 %   Internal
 %%%%
 
-collapse_features_map(Map) ->
-    Now = erlang:system_time(seconds),
-    CollapseFun = fun(K, V, AccIn) ->
-      NewV = collapse_to_boolean(V, Now),
-      maps:put(K, NewV, AccIn)
-    end,
-    maps:fold(CollapseFun, #{}, Map).
+
+% Compares the Ran float (0..1) to the progress of time through the rollout
+% interval true if Rand is less than the progress, false otherwise
+collapse_rollout_progress(Start, End, Now, Rand) ->
+    Duration = End - Start,
+    ProgressTime = Now - Start,
+    Chance = ProgressTime / Duration,
+
+    Boolean = Rand =< Chance,
+    Boolean.
--- tests/features_test.erl
+++ tests/features_test.erl
@@ -11,25 +11,40 @@ collapse_to_boolean_with_boolean_test() ->
     TrueSpec = test_utils:defaulted_feature_spec(
         #{boolean => true}),
 
-    ?assertEqual(false, ?MUT:collapse_to_boolean(FalseSpec, 0)),
-    ?assertEqual(true, ?MUT:collapse_to_boolean(TrueSpec, 0)),
+    ?assertEqual(false, ?MUT:collapse_to_boolean(FalseSpec, 0, 0)),
+    ?assertEqual(true, ?MUT:collapse_to_boolean(TrueSpec, 0, 0)),
 
     unload().
 
 collapse_to_boolean_with_rollout_test() ->
     load(),
     Now = erlang:system_time(seconds),
+    Rand = 0.5,
 
+    % Before the start of the rollout
     BeforeSpec = test_utils:defaulted_feature_spec(
         #{rollout_start => Now + 10,
           rollout_end => Now + 60}),
 
+    % Not yet passed 50% of the rollout
+    NotReachedSpec = test_utils:defaulted_feature_spec(
+        #{rollout_start => Now -5,
+          rollout_end => Now + 10}),
+
+    % Passed 50% of the rollout
+    ReachedSpec = test_utils:defaulted_feature_spec(
+        #{rollout_start => Now - 10,
+          rollout_end => Now - 5}),
+
+    % After the end of the rollout
     AfterSpec = test_utils:defaulted_feature_spec(
         #{rollout_start => Now - 60,
           rollout_end => Now - 10}),
 
-    ?assertEqual(false, ?MUT:collapse_to_boolean(BeforeSpec, Now)),
-    ?assertEqual(true, ?MUT:collapse_to_boolean(AfterSpec, Now)),
+    ?assertEqual(false, ?MUT:collapse_to_boolean(BeforeSpec, Now, Rand)),
+    ?assertEqual(false, ?MUT:collapse_to_boolean(NotReachedSpec, Now, Rand)),
+    ?assertEqual(true, ?MUT:collapse_to_boolean(ReachedSpec, Now, Rand)),
+    ?assertEqual(true, ?MUT:collapse_to_boolean(AfterSpec, Now, Rand)),
 
     unload().
 
@@ -58,7 +73,11 @@ collapse_features_map_test() ->
 
 
 load() ->
+    meck:new(rand, [unstick]),
+    meck:expect(rand, uniform, [], 0.5),
     ok.
 
 unload() ->
+    true = meck:validate(rand),
+    meck:unload(rand),
     ok.
