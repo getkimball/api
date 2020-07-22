78d317e4d26009964e8cc2c5e746d09b9a21547f
Philip Cristiano <git@philipcristiano.com>
2020-07-22 16:06:05 -0400

    tf: Add bucket to github secrets
    So that it can eventually be used in the deploy process

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "app_bucket" {
   bucket = "getkimball-features-app"
   acl    = "private"
 
@@ -6,4 +6,10 @@ resource "aws_s3_bucket" "b" {
     Name        = "getkimball-features-app"
     Environment = "dev"
   }
+}
+
+resource "github_actions_secret" "s3_bucket" {
+  repository      = "features"
+  secret_name     = "S3_BUCKET"
+  plaintext_value = aws_s3_bucket.app_bucket.id
 }
