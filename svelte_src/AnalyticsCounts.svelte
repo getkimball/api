cc58c517b2020db6b65a56e505a5afd0f0ff8fda
Philip Cristiano <git@philipcristiano.com>
2020-08-21 15:45:21 -0400

    ui: Generate predictions for goals given events
    https://en.wikipedia.org/wiki/Bayes%27_theorem

--- svelte_src/AnalyticsCounts.svelte
+++ svelte_src/AnalyticsCounts.svelte
@@ -5,12 +5,39 @@
              Table} from "sveltestrap";
 
     import AnalyticsCountItem from './AnalyticsCountItem.svelte';
+    import AnalyticsPrediction from './AnalyticsCountItem.svelte';
     let analytics = [];
+    let analyticLookup = {};
+    let goals = [];
+    let predictions = [];
 
     onMount(async () => {
         const res = await fetch(`v0/analytics`);
         const json = await res.json();
         analytics = json.counts.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase())
+
+        goals = analytics.filter(obj => obj.single_event_counts.length > 0);
+
+        analyticLookup = analytics.reduce(function(map, obj) {
+            map[obj.name] = obj.count;
+            console.log(obj.name);
+            return map;
+        }, {});
+
+        let global_count = analyticLookup["global_counter"];
+
+        let goal;
+        let event;
+        let probability;
+        for (goal of goals) {
+          for (event of goal.single_event_counts) {
+
+            probability = ((event.count / goal.count) * (goal.count / global_count)) / (analyticLookup[event.event] / global_count);
+            predictions = [...predictions, {"goal": goal.name, "event": event.event, "probability": probability}];
+          }
+
+        }
+
     });
 
 </script>
@@ -30,7 +57,16 @@
     No feature analytics yet
     Make sure "user_id" and "feature" are set
 {/each}
-
-
 </Col>
 </Row>
+
+{#each predictions as prediction }
+<Row>
+  <Col></Col>
+  <Col xs="6">
+  {prediction.goal} - {prediction.event} - {prediction.probability}
+  <br />
+  </Col>
+  <Col></Col>
+</Row>
+{/each}
