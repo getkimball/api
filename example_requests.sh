474c900948b80bd00e30d02ac5936a66a9420da7
Philip Cristiano <git@philipcristiano.com>
2020-12-14 10:20:28 -0500

    example_requests: Use namespaces

--- example_requests.sh
+++ example_requests.sh
@@ -6,37 +6,39 @@ export CTX=$(echo '{"feature": "features_post"}' | base64)
 #curl -vf "http://127.0.0.1:8080/v0/features?user_obj=$USER&context_obj=$CTX"
 
 HOST=http://127.0.0.1:8080
-DS_HOST=http://127.0.0.1:8081
 
 curl -f -H "content-type: application/json" \
         -d "{\"goal_name\": \"goal\"}" \
      "${HOST}/v0/goals"
 
 send_event () {
-    EVENT_NAME=$1
-    USER_ID=$2
+    NAMESPACE=$1
+    EVENT_NAME=$2
+    USER_ID=$3
 
     curl -f -H "content-type: application/json" \
-            -d "{\"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\"}" \
+            -d "{\"namespace\": \"${NAMESPACE}\", \"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\"}" \
             "${HOST}/v0/analytics"
 }
 
 send_event_value () {
-    EVENT_NAME=$1
-    USER_ID=$2
-    VALUE=$3
+    NAMESPACE=$1
+    EVENT_NAME=$2
+    USER_ID=$3
+    VALUE=$4
 
     curl -f -H "content-type: application/json" \
-            -d "{\"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\", \"value\": ${VALUE} }" \
+            -d "{\"namespace\": \"${NAMESPACE}\", \"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\", \"value\": ${VALUE} }" \
             "${HOST}/v0/analytics"
 }
 
 send_goal_event () {
-    EVENT_NAME=$1
-    USER_ID=$2
+    NAMESPACE=$1
+    EVENT_NAME=$2
+    USER_ID=$3
 
     curl -f -H "content-type: application/json" \
-            -d "{\"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\", \"ensure_goal\": true }" \
+            -d "{\"namespace\": \"${NAMESPACE}\", \"event_name\": \"${EVENT_NAME}\", \"user_id\": \"${USER_ID}\", \"ensure_goal\": true }" \
             "${HOST}/v0/analytics"
 }
 
@@ -50,19 +52,19 @@ generic_example () {
     for i in $(seq 1 $END); do
         R=$(( ( RANDOM % $RAND_MOD )  + 1 ))
         echo $i - $R of $RAND_MOD
-        send_event analytics_post "${i}"
-        send_event_value value "${i}" 1
+        send_event default analytics_post "${i}"
+        send_event_value default value "${i}" 1
 
         curl -f -H "content-type: application/json" \
                 -d "{ \"events\": [{\"event_name\": \"multi_submit_1\", \"user_id\": \"$i\"},{\"event_name\": \"multi_submit_2\", \"user_id\": \"$i\"}]}" \
                 "${HOST}/v0/analytics"
 
-        send_event "step ${R}" ${i}
-        send_goal_event "analytics_post_goal" "${i}"
-        send_event "goal" "${i}"
+        send_event default "step ${R}" ${i}
+        send_goal_event default "analytics_post_goal" "${i}"
+        send_event default "goal" "${i}"
 
         if (( RANDOM % 2 )); then
-            send_goal_event "chance_goal" "${i}"
+            send_goal_event default "chance_goal" "${i}"
         fi
 
     done
@@ -74,22 +76,22 @@ saas_example() {
         P=$(( ( RANDOM % 100 )  + 1 ))
         echo "${i}"
 
-        send_event view_any_page "${i}"
+        send_event saas_example view_any_page "${i}"
 
         if [[ $P -le 80 ]]; then
-            send_goal_event "signup" "${i}"
+            send_goal_event saas_example "signup" "${i}"
         fi
 
         if [[ $P -le 75 ]]; then
-            send_goal_event "login" "${i}"
+            send_goal_event saas_example "login" "${i}"
         fi
 
         if [[ $P -le 60 ]]; then
-            send_goal_event "feature_a" "${i}"
+            send_goal_event saas_example "feature_a" "${i}"
         fi
 
         if [[ $P -ge 50 ]]; then
-            send_goal_event "feature_b" "${i}"
+            send_goal_event saas_example "feature_b" "${i}"
         fi
 
     done
@@ -104,21 +106,21 @@ non_user_example() {
         OTHER=$(( ( RANDOM % 3 )  + 1 ))
         echo "${i}" "${CUSTOMER}" "${OTHER}"
 
-        send_event created "${i}"
-        send_event "object-type: invoice" "${i}"
-        send_event "customer-id: ${CUSTOMER}" "${i}"
+        send_event invoice_example created "${i}"
+        send_event invoice_example "object-type: invoice" "${i}"
+        send_event invoice_example "customer-id: ${CUSTOMER}" "${i}"
 
         if [[ $OTHER = 1 ]]; then
-            send_event "status: created" "${i}"
+            send_event invoice_example "status: created" "${i}"
         fi
         if [[ $OTHER = 2 ]]; then
-            send_event "status: created" "${i}"
-            send_event "status: sent" "${i}"
+            send_event invoice_example "status: created" "${i}"
+            send_event invoice_example "status: sent" "${i}"
         fi
         if [[ $OTHER = 3 ]]; then
-            send_event "status: created" "${i}"
-            send_event "status: sent" "${i}"
-            send_goal_event "status: paid" "${i}"
+            send_event invoice_example "status: created" "${i}"
+            send_event invoice_example "status: sent" "${i}"
+            send_goal_event invoice_example "status: paid" "${i}"
         fi
 
     done
@@ -126,5 +128,5 @@ non_user_example() {
 
 
 generic_example
-# saas_example
-# non_user_example
+saas_example
+non_user_example
