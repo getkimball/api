bcd5d7276497f2ab4b95696e4e07e5527c51cdd6
Philip Cristiano <git@philipcristiano.com>
2021-05-13 08:43:24 -0400

    auth: Exclude ok handler
    So that enabling auth doesn't break kubernetes healthchecks. If there is
    some constant header put through, the lack of authorization for handler
    routes means the healthcheck credentials could be use for any operation

--- tests/cb_auth_simple_header_test.erl
+++ tests/cb_auth_simple_header_test.erl
@@ -15,21 +15,26 @@ bearer_auth_test_() ->
     {foreach, fun load/0, fun unload/1, [
         fun bearer/0,
         fun bearer_no_header/0,
-        fun bearer_no_tokens/0
+        fun bearer_no_tokens/0,
+        fun exclude_handler/0
     ]}.
 
 bearer() ->
     Token = <<"foo">>,
     Headers = #{
-        <<"Authorization">> => << <<"Bearer ">>/binary, Token/binary >>
+        <<"Authorization">> => <<<<"Bearer ">>/binary, Token/binary>>
     },
     Req = ?CTH:req(<<"GET">>, #{headers => Headers}),
 
-    Env = #{cb_auth => #{auth_tokens => [Token],
-                         unauth_handler => unauth_handler}},
+    Env = #{
+        cb_auth => #{
+            auth_tokens => [Token],
+            unauth_handler => unauth_handler
+        },
+        handler => test_handler
+    },
 
     MiddlewareResp = ?MUT:execute(Req, Env),
-
 
     ?assertEqual({ok, Req, Env}, MiddlewareResp).
 
@@ -38,28 +43,58 @@ bearer_no_header() ->
     Headers = #{},
     Req = ?CTH:req(<<"GET">>, #{headers => Headers}),
 
-    Env = #{cb_auth => #{auth_tokens => [Token],
-                         unauth_handler => unauth_handler}},
+    Env = #{
+        cb_auth => #{
+            auth_tokens => [Token],
+            unauth_handler => unauth_handler
+        },
+        handler => test_handler
+    },
 
     MiddlewareResp = ?MUT:execute(Req, Env),
 
     ExpectedEnv = Env#{handler => unauth_handler},
-
 
     ?assertEqual({ok, Req, ExpectedEnv}, MiddlewareResp).
 
 bearer_no_tokens() ->
     Token = <<"foo">>,
     Headers = #{
-        <<"Authorization">> => << <<"Bearer ">>/binary, Token/binary >>
+        <<"Authorization">> => <<<<"Bearer ">>/binary, Token/binary>>
     },
     Req = ?CTH:req(<<"GET">>, #{headers => Headers}),
 
-    Env = #{cb_auth => #{auth_tokens => [],
-                         unauth_handler => unauth_handler}},
+    Env = #{
+        cb_auth => #{
+            auth_tokens => [],
+            unauth_handler => unauth_handler
+        },
+        handler => test_handler
+    },
 
     MiddlewareResp = ?MUT:execute(Req, Env),
 
     ExpectedEnv = Env#{handler => unauth_handler},
+
+    ?assertEqual({ok, Req, ExpectedEnv}, MiddlewareResp).
+
+exclude_handler() ->
+    Token = <<"foo">>,
+    Headers = #{},
+    Req = ?CTH:req(<<"GET">>, #{headers => Headers}),
+    Handler = test_handler,
+
+    Env = #{
+        cb_auth => #{
+            auth_tokens => [Token],
+            exclude_handlers => [Handler],
+            unauth_handler => unauth_handler
+        },
+        handler => Handler
+    },
+
+    MiddlewareResp = ?MUT:execute(Req, Env),
+
+    ExpectedEnv = Env#{handler => Handler},
 
     ?assertEqual({ok, Req, ExpectedEnv}, MiddlewareResp).
